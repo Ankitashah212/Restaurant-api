4cb6d1f9a660f3624ee386f04317a63440856141
Tara <tara@nelsonwriting.com>
2017-09-29 12:25:58 -0400

    Set up express and body parser

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ var server = http.createServer(handleRequest);
 var app = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+//Set up express app
+
+var app = express();
+var PORT = 3000;
+
+// Parse data via express app
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//  (DATA)
+// =============================================================
+var characters = [
+    {
+      routeName: "yoda",
+      name: "Yoda",
+      role: "Jedi Master",
+      age: 900,
+      forcePoints: 2000
+    }
+]
